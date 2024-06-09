585840716df242c618acae2b578c918271eb297d
Dre Suguitan <ddre2026@gmail.com>
2024-06-08 23:54:53 -0700

    yes

--- Lab6/Cache.java
+++ Lab6/Cache.java
@@ -1,20 +1,68 @@
 import java.util.*;
 
 
-public class Cache {
+public class Cache{
     private CacheRow[] cache;
-    private int cacheSize;
     private int blockSize;
     private int numLines;
+    private int storageInKB;
+    private int associativity;
 
-    public Cache(int storageInKB, 
-        int associativity, 
-        int blockWordLength){
+    private int indexBitLength;
+    private int blockOffSetLength;
+
+    public Cache(
+            int storageInKB, 
+            int associativity, 
+            int blockWordLength
+        ){
         
+        this.storageInKB = storageInKB;
+        this.associativity = associativity;
         blockSize = blockWordLength;
-        
+
+        numLines = (int) (Math.pow(2, 10) * storageInKB / associativity / (blockWordLength * 4));
+        cache = new CacheRow[numLines];        
+        Arrays.fill(cache, new CacheRow(associativity));
+
+        indexBitLength = log2(numLines);
+        blockOffSetLength = log2(blockWordLength);
     }
+
+    public int getStorage(){
+        return storageInKB;
+    }
+    public int getBlockSize(){
+        return blockSize;
+    }
+    public int getAssociativity(){
+        return associativity;
+    }
+
+    public boolean analyzeAddress(int address){
+        boolean wasHit = false;
+
+        // find tag
+        int shiftLeftBy = 2 + indexBitLength + blockOffSetLength;
+        int tag = address << shiftLeftBy;
+
+        // find index
+        shiftLeftBy = 2 + blockOffSetLength;
+        int index = (address << shiftLeftBy) % numLines;
+
+        // is tag at index (any where)
+        wasHit = cache[index].hitOrMiss(tag);
+
+        return wasHit;
+    }
+
+    private int log2(int x){
+        return (int) (Math.log(x) / Math.log(2));
+    }
+
 }
+
+
 
 /* 
  * 2, 1, 1 
--- Lab6/CacheRow.java
+++ Lab6/CacheRow.java
@@ -1,7 +1,60 @@
 import java.util.*;
 
 public class CacheRow {
-    private boolean valid;
-    private int tag;
-    private ArrayList<Integer> LastUsed;
+    private int[] tags;
+    private ArrayList<Integer> lastUsed;
+
+    public CacheRow(int associativity){
+        tags = new int[associativity];
+        Arrays.fill(tags, -1);
+        lastUsed = new ArrayList<Integer>();
+    }
+
+    public int getTagAt(int aOffset){
+        return tags[aOffset];
+    }
+
+    public int[] getTags(){
+        return tags;
+    }
+
+    public void setTagAt(int tag, int aOffset){
+        tags[aOffset] = tag;
+    }
+
+    public boolean updateLastUsed(int aOffset){
+        boolean removed = lastUsed.remove((Integer) aOffset);
+        lastUsed.add(aOffset);
+
+        return removed;
+    }
+
+    public int leastRecentlyUsed(){
+        return lastUsed.get(0);
+    }
+
+    public boolean hitOrMiss(int tag){
+        int aOffset = Arrays.binarySearch(tags, tag);
+        
+        if(aOffset == -1){
+            int unusedIndex = Arrays.binarySearch(tags, -1);
+
+            if(unusedIndex == -1){
+                tags[leastRecentlyUsed()] = tag;
+                updateLastUsed(leastRecentlyUsed());
+            } else {
+                tags[unusedIndex] = tag;
+                int modified = lastUsed.remove(0);
+                lastUsed.add(modified);
+            }
+
+
+            return false;
+        } else {
+           updateLastUsed(aOffset);
+           return true;
+        }
+    }
+
+    
 }
